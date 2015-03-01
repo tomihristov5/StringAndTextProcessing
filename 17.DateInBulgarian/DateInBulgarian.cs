befc9c7b01ec286eb2858143361d964a0d2cd352
tomihristov5 <hrist0v@abv.bg>
2015-03-01 21:02:27 +0200

    hb

--- 17.DateInBulgarian/DateInBulgarian.cs
+++ 17.DateInBulgarian/DateInBulgarian.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Problem 17. Date in Bulgarian
+// Write a program that reads a date and time given in the format: day.month.year hour:minute:second and prints the date and time after 6 hours and 30 minutes (in the same format) along with the day of week in Bulgarian.
+
+
+using System;
+using System.Globalization;
 
 namespace _17.DateInBulgarian
 {
@@ -6,7 +11,13 @@ namespace _17.DateInBulgarian
     {
         static void Main()
         {
-
+            Console.Write("Enter date (d.M.yyyy H:mm:ss): ");
+            string input = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(input, "d.M.yyyy H:mm:ss", CultureInfo.InvariantCulture);
+            date = date.AddHours(6);
+            date = date.AddMinutes(30);
+            Console.WriteLine("Date after 6 hours and 30 mins and day of week: \n {0}", 
+                date.ToString("dddd dd.MM.yyyy HH:mm:ss", new CultureInfo("bg-BG")));
         }
     }
 }
---
+++ 18.ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,33 @@
+﻿// Problem 18. Extract e-mails
+// Write a program for extracting all email addresses from given text.
+// All sub-strings that match the format <identifier>@<host>…<domain> should be recognized as emails.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _18.ExtractEmails
+{
+    class ExtractEmails
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter some text with some e-mail addressed in it: ");
+            string text = Console.ReadLine();
+            string[] words = text.Split(new string[] { " ", ";", ",", ". " }, StringSplitOptions.RemoveEmptyEntries);
+            string regex = @"^([a-zA-Z0-9_\-][a-zA-Z0-9_\-\.]{0,49})" +
+                       @"@(([a-zA-Z0-9][a-zA-Z0-9\-]{0,49}\.)+[a-zA-Z]{2,4})$";
+            Console.WriteLine("Extracted e-mail addresses:");
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (Regex.IsMatch(words[i], regex))
+                {
+                    Console.WriteLine(" - {0}", words[i]);
+                }
+            }
+        }
+    }
+}
